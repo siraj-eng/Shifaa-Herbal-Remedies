b7f80190c28398c7f0780c714e75ff1e7026b634
siraj-eng <wahhajsiraj16@gmail.com>
2025-02-24 15:08:40 +0300

    third Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript to toggle the menu
+document.getElementById("menu-icon").addEventListener("click", function() {
+    document.getElementById("nav-menu").classList.toggle("active");
+});
